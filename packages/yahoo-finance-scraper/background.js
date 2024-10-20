b06ca44d89822ad3bdca3f515c038b8a6f3df8d5
Akhilesh Yadav <akhilesh84@gmail.com>
2024-10-20 23:32:22 +0530

    .

--- packages/yahoo-finance-scraper/background.js
+++ packages/yahoo-finance-scraper/background.js
@@ -5,10 +5,9 @@ chrome.action.onClicked.addListener((tab) => {
     }, () => {
         let res = chrome.tabs.sendMessage(tab.id, { action: "scrape" });
         res.then(data => {
-            console.table(data.content);
+            // console.table(data.content);
             // Save the data to a CSV file using chrole.download API
             const csv = data.content.map(row => Object.values(row).join(",")).join("\n");
-            const blob = new Blob([csv], { type: "text/csv" });
             
             // const url = URL.createObjectURL(blob);
             chrome.downloads.download({
